36074004916dbd503824fb20a3883be6f49db8de
wang190 <89770062+wangwang190@users.noreply.github.com>
2022-06-29 11:30:11 +0800

    added item count icon at "Cart"

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
+import { Store } from '../utils/Store';
 
 const Layout = ({ children, title }) => {
+  const {state, dispatch} = useContext(Store);
+  const {cart} = state;
   return (
-    <div>
+    <>
       <Head>
         <title>{title ? title + 'Bamazon' : 'Bamazon'}</title>
         <meta name="description" content="E-commerce website" />
@@ -19,7 +22,13 @@ const Layout = ({ children, title }) => {
             </Link>
             <div>
               <Link href="/cart">
-                <a className="p-2">Cart</a>
+                <a className="p-2">
+                  Cart
+                  {cart.cartItems.length > 0 && (
+                  <span className="ml-1 rounded-full bg-red-600 px-2 py-1 text-xs font-bold text-white">
+                    {cart.cartItems.reduce((a,c) => a + c.quantity, 0)}
+                  </span>)}
+                </a>
               </Link>
               <Link href="/login">
                 <a className="p-2">Login</a>
@@ -33,7 +42,7 @@ const Layout = ({ children, title }) => {
           <p> Copyright &copy; 2022 Bamazon</p>
         </footer>
       </div>
-    </div>
+    </>
   );
 };
 
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
+import { Storeprovider } from '../utils/Store'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <Storeprovider>
+    <Component {...pageProps} />
+  </Storeprovider>
+  );
 }
 
 export default MyApp
